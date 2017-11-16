e8778f151c9135354827abef55ee5a6e54419541
Robindekogel <31471377+Robindekogel@users.noreply.github.com>
2017-11-16 11:31:36 +0100

    Add files via upload

---
+++ Java1.js
@@ -0,0 +1,12 @@
+
+var auto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "Blauw"
+};
+
+console.log(auto.kleur);
+document.getElementById("grotekop").innerHTML = auto.merk
+
+
---
+++ vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subkop").innerText = "Mijn geweldige subkop";
+document.getElementById("plaatje1").src = "img/plaatje1.jpg";
+document.getElementById("plaatje2").src = "img/plaatje2.jpg";
